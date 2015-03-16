ea10211786ff152f59dd16c514cae762315521bd
somewrite開発用 <somewrite@yanqirenshi-no-MacBook-Air.local>
2015-03-17 08:48:53 +0900

    add src/picture-mode.el. close #12

--- init.el
+++ init.el
@@ -18,91 +18,66 @@
 ;;;;;
 
 ;;;;;
-;;;;; package maneger
+;;;;; Setting Mode
 ;;;;;
+
+;; package maneger
 (when (>= emacs-major-version 24)
   (load "~/prj/emacs/src/package.el"))
 
-;;;;;
-;;;;; 環境変数の設定
-;;;;;
-;; OSの環境変数の引き継ぎ
-(load "~/prj/emacs/src/path.el")
-;; Mac のみ。
-(when (eq window-system 'ns)
+;; 環境変数の設定
+(load "~/prj/emacs/src/path.el") ;; OSの環境変数の引き継ぎ
+(when (eq window-system 'ns) ;; Mac のみ。
   (setenv "PS1" "\\w$ ")
   (setenv "LANG" "ja_JP.UTF-8"))
 
-;;;;;
-;;;;; emacs の設定あれこれ
-;;;;;
+;; emacs の設定あれこれ
 (load "~/prj/emacs/src/environment.el")
 ;; (load "~/prj/emacs/src/emacs-etc.el") 廃棄予定。
 
-;;;;;
-;;;;; 色設定
-;;;;;
+;; 色設定
 (if (>= emacs-major-version 24)
     (load "~/prj/emacs/src/theme.el")
-    (load "~/prj/emacs/src/color-theme.el"))
+  (load "~/prj/emacs/src/color-theme.el"))
 
-;;;;;
-;;;;; フォント設定
-;;;;;
+;; フォント設定
 (when (eq window-system 'ns)
   (load "~/prj/emacs/src/font-mac.el"))
 
-;;;;;
-;;;;; markdown-mode
-;;;;;
+;; picture-mode
+(load "~/prj/emacs/src/picture-mode.el")
+
+;; markdown-mode
 ;; (load "~/prj/emacs/src/markdown-mode.el")
 
-;;;;;
-;;;;; slime
-;;;;;
-;;;;; ex) (defvar *iwo-cl-path* "/usr/local/bin/sbcl")
-;;;;;     (defvar *iwo-slime-path* "~/prj/slime")
-(defvar *iwo-cl-path*    nil)
-(defvar *iwo-slime-path* nil)
+;; slime
+(defvar *iwo-cl-path*    nil) ;; ex) (defvar *iwo-cl-path* "/usr/local/bin/sbcl")
+(defvar *iwo-slime-path* nil) ;; ex) (defvar *iwo-slime-path* "~/prj/slime")
 ;; (load "~/prj/emacs/src/slime.el")
 
-;;;
-;;; open junk file
-;;;
+;; open junk file
 ;; (load "~/prj/emacs/src/open-junk-file.el")
 
-;;;;;
-;;;;; javascript
-;;;;;
-;; js2-mode
-;;(load "~/prj/emacs/src/js2-mode.el")
-;; web-mode
-;;(load "~/prj/emacs/src/web-mode.el")
+;; javascript
+;;(load "~/prj/emacs/src/js2-mode.el") ;; js2-mode
+;;(load "~/prj/emacs/src/web-mode.el") ;; web-mode
 
-;;;;;
-;;;;; ddskk
-;;;;;
+;; ddskk
 ;; (load "~/prj/emacs/src/ddskk.el")
 
-;;;;;
-;;;;; w3m
-;;;;;
+;; w3m
 ;; (load "~/prj/emacs/src/w3m.el")
 
-;;;;;
-;;;;; tabbar
-;;;;;
+;; tabbar
 ;; (load "~/prj/emacs/src/tabbar.el")
 
-;;;;;
-;;;;; navi2ch
-;;;;;
+;; navi2ch
 ;; (load "~/prj/emacs/src/navi2ch.el")
 
-;;;;;
-;;;;; twitter
-;;;;;
+;; twitter
 ;; (load "~/prj/emacs/src/twittering-mode.el")
+
+
 
 ;;;;;
 ;;;;; Mya Lisp library
---
+++ src/picture-mode.el
@@ -0,0 +1,6 @@
+;;;;;
+;;;;; 拡張子の関連づけ
+;;;;;
+(setq auto-mode-alist
+      (append '(("\\.pic$" . picture-mode))
+              auto-mode-alist))
