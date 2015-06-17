9fc48e0d112d5f0b5f09282547e2677f5782f71e
yanqirenshi <yanqirenshi@gmail.com>
2015-06-18 07:40:16 +0900

    modify init.el

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 ;;;;; Contents
 ;;;;; --------
 ;;;;;  1. Setting Mode
-;;;;;     - emacs の設定あれこれ
 ;;;;;     - package maneger
 ;;;;;     - 環境変数の設定
+;;;;;     - emacs の設定あれこれ
 ;;;;;     - 色設定
 ;;;;;     - フォント設定
 ;;;;;     - picture-mode
@@ -13,7 +13,7 @@
 ;;;;;     - open junk file
 ;;;;;     - javascript
 ;;;;;     - ddskk
-;;;;;     - eww
+;;;;;     - w3m
 ;;;;;     - tabbar
 ;;;;;     - navi2ch
 ;;;;;     - twitter
@@ -26,54 +26,47 @@
 ;;;;;
 ;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; emacs の設定あれこれ
+;; emacs の設定あれこれ
 (load "~/prj/emacs/src/language.el")   ;; 言語/文字コード
 (load "~/prj/emacs/src/environment.el")
 (load "~/prj/emacs/src/mode-line.el")  ;; mode-line の設定
 
-;;; package maneger
+;; package maneger
 (when (>= emacs-major-version 24)
   (load "~/prj/emacs/src/package.el"))
 
-;;; 環境変数の設定
+;; 環境変数の設定
 (load "~/prj/emacs/src/path.el") ;; OSの環境変数の引き継ぎ
 (when (eq window-system 'ns) ;; Mac のみ。
   (setenv "PS1" "\\w$ ")
   (setenv "LANG" "ja_JP.UTF-8"))
 
-;;; emacs の設定あれこれ
-(load "~/prj/emacs/src/environment.el")
-(load "~/prj/emacs/src/language.el")   ;; 言語/文字コード
-(load "~/prj/emacs/src/mode-line.el")  ;; mode-line の設定
-
-;;; 色設定
+;; 色設定
 (load "~/prj/emacs/src/buffer.el")
 
-;;; 色設定
+;; 色設定
 (if (>= emacs-major-version 24)
     (load "~/prj/emacs/src/theme.el")
   (load "~/prj/emacs/src/color-theme.el"))
 
-;;; フォント設定
+;; フォント設定
 (when (eq window-system 'ns)
   (load "~/prj/emacs/src/font-mac.el"))
 
-;;; picture-mode
+;; picture-mode
 (load "~/prj/emacs/src/picture-mode.el")
 
-;;; org-mode
+;; org-mode
 (load "~/prj/emacs/src/org-mode.el")
 
 ;; markdown-mode
 (load "~/prj/emacs/src/markdown-mode.el")
 
-;;; slime
-;; (defvar *iwo-cl-path*    nil) 
-;;    ex) (defvar *iwo-cl-path* "/usr/local/bin/sbcl") 
-;;        (defvar *iwo-cl-path* "ros run -R")
-;; (defvar *iwo-slime-path* nil) 
-;;    ex) (defvar *iwo-slime-path* "~/prj/slime")
-;; (load "~/prj/emacs/src/slime.el")
+;; slime
+(add-to-list 'load-path (expand-file-name "~/prj/slime-repl-ansi-color"))
+(defvar *iwo-cl-path* "ros run -R")
+(defvar *iwo-slime-path* "/Users/yanqirenshi/prj/slime")
+(load "~/prj/emacs/src/slime.el")
 
 ;; open junk file
 (load "~/prj/emacs/src/open-junk-file.el")
@@ -89,7 +82,7 @@
 (load "~/prj/emacs/src/eww.el")
 
 ;; tabbar
-(load "~/prj/emacs/src/tabbar.el")
+;; (load "~/prj/emacs/src/tabbar.el")
 
 ;; navi2ch
 ;; (load "~/prj/emacs/src/navi2ch.el")
@@ -99,6 +92,9 @@
 
 ;; T-ER Mode
 (load "~/prj/emacs/src/ter-mode.el")
+
+;; git-gutter
+(load "~/prj/emacs/src/git-gutter.el")
 
 
 ;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
