3f84722d601861cd0f8181aa0ea42681586a33fd
yanqirenshi <yanqirenshi@gmail.com>
2016-04-08 10:46:28 +0900

    add orgtbl-mode 4 markdown-mode

--- src/markdown-mode.el
+++ src/markdown-mode.el
@@ -17,3 +17,16 @@
           (lambda () (auto-fill-mode -1)))
 
 (add-hook 'markdown-mode-hook 'turn-on-orgtbl)
+
+;;;
+;;; Markdown で表(テーブル)を描く
+;;; http://qiita.com/nanasess/items/c9342c06a3e28e64aeb8
+;;;
+(defun cleanup-org-tables ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (search-forward "-+-" nil t) (replace-match "-|-"))))
+(add-hook 'markdown-mode-hook 'orgtbl-mode)
+(add-hook 'markdown-mode-hook
+          #'(lambda()
+          (add-hook 'after-save-hook 'cleanup-org-tables  nil 'make-it-local)))
