9c2448590cf0ac67784129131195fee1e8c15e3f
Fool's Gold <fg@Satoshi-no-MacBook-Air.local>
2016-04-22 14:17:49 +0900

    change directory

--- init.el
+++ init.el
@@ -9,9 +9,9 @@
 ;;;;; 1. Mya Lisp library
 ;;;;;
 ;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
-(load "~/prj/emacs/lisp/my.el")
-(load "~/prj/emacs/lisp/date.el")
-(load "~/prj/emacs/lisp/dir-and-file.el")
+(load "~/.emacs.d/dist/emacs/lisp/my.el")
+(load "~/.emacs.d/dist/emacs/lisp/date.el")
+(load "~/.emacs.d/dist/emacs/lisp/dir-and-file.el")
 
 ;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;
@@ -20,25 +20,25 @@
 ;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(load "~/prj/emacs/src/language.el")                               ;; 言語/文字コード
-(load "~/prj/emacs/src/environment.el")                            ;; emacs の設定あれこれ
-(load "~/prj/emacs/src/cursor.el")                                 ;; cursor の設定あれこれ
-(load "~/prj/emacs/src/mode-line.el")                              ;; mode-line の設定
-                                                                   ;; (load "~/prj/emacs/src/package.el") ;; package maneger
-(load "~/prj/emacs/src/el-get.el")                                 ;; package maneger
-(load "~/prj/emacs/src/path.el")                                   ;; 環境変数の設定(OSの環境変数の引き継ぎ)
-(load "~/prj/emacs/src/buffer.el")                                 ;; buffer 設定
-(load "~/prj/emacs/src/theme.el")                                  ;; 色設定
-(load "~/prj/emacs/src/picture-mode.el")                           ;; picture-mode
-(load "~/prj/emacs/src/org-mode.el")                               ;; org-mode
-(load "~/prj/emacs/src/markdown-mode.el")                          ;; markdown-mode
-(load "~/prj/emacs/src/slime.el")                                  ;; slime
-(load "~/prj/emacs/src/open-junk-file.el")                         ;; open junk file
-(load "~/prj/emacs/src/ddskk.el")                                  ;; ddskk
-(load "~/prj/emacs/src/eww.el")                                    ;; eww
-(load "~/prj/emacs/src/js2-mode.el")                               ;; js2-mode
-(load "~/prj/emacs/src/web-mode.el")                               ;; web-mode
-(load "~/prj/emacs/src/twittering-mode.el")                        ;; twitter
-(load "~/prj/emacs/src/ter-mode.el")                               ;; T-ER Mode
-(load "~/prj/emacs/src/git-gutter.el")                             ;; git-gutter
-(when (eq window-system 'ns) (load "~/prj/emacs/src/font-mac.el")) ;; フォント設定
+(load "~/.emacs.d/dist/emacs/src/language.el")          ;; 言語/文字コード
+(load "~/.emacs.d/dist/emacs/src/environment.el")       ;; emacs の設定あれこれ
+(load "~/.emacs.d/dist/emacs/src/cursor.el")            ;; cursor の設定あれこれ
+(load "~/.emacs.d/dist/emacs/src/mode-line.el")         ;; mode-line の設定
+                                                        ;; (load "~/.emacs.d/dist/emacs/src/package.el") ;; package maneger
+(load "~/.emacs.d/dist/emacs/src/el-get.el")            ;; package maneger
+(load "~/.emacs.d/dist/emacs/src/path.el")              ;; 環境変数の設定(OSの環境変数の引き継ぎ)
+(load "~/.emacs.d/dist/emacs/src/buffer.el")            ;; buffer 設定
+(load "~/.emacs.d/dist/emacs/src/theme.el")             ;; 色設定
+(load "~/.emacs.d/dist/emacs/src/picture-mode.el")      ;; picture-mode
+(load "~/.emacs.d/dist/emacs/src/org-mode.el")          ;; org-mode
+(load "~/.emacs.d/dist/emacs/src/markdown-mode.el")     ;; markdown-mode
+(load "~/.emacs.d/dist/emacs/src/slime.el")             ;; slime
+(load "~/.emacs.d/dist/emacs/src/open-junk-file.el")    ;; open junk file
+(load "~/.emacs.d/dist/emacs/src/ddskk.el")             ;; ddskk
+(load "~/.emacs.d/dist/emacs/src/eww.el")               ;; eww
+(load "~/.emacs.d/dist/emacs/src/js2-mode.el")          ;; js2-mode
+(load "~/.emacs.d/dist/emacs/src/web-mode.el")          ;; web-mode
+(load "~/.emacs.d/dist/emacs/src/twittering-mode.el")   ;; twitter
+(load "~/.emacs.d/dist/emacs/src/ter-mode.el")          ;; T-ER Mode
+(load "~/.emacs.d/dist/emacs/src/git-gutter.el")        ;; git-gutter
+(when (eq window-system 'ns) (load "~/.emacs/dist/emacs/src/font-mac.el")) ;; フォント設定
