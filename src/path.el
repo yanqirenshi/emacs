3e17dc7033b36553b46c79362a1c06190ba746e7
岩崎仁是 <yanqirenshi@gmail.com>
2016-07-03 13:36:36 +0900

    modify path.el

--- src/path.el
+++ src/path.el
@@ -1,14 +1,15 @@
 ;;;;;
 ;;;;; setting path
 ;;;;;
+;;;;; terminal の PATH を引き継ぐ。
+;;;;;   参考: http://qiita.com/catatsuy/items/3dda714f4c60c435bb25
+;;;;;
 
+;; windwos は実行しない
+(unless (eq window-system 'w32)
+  (exec-path-from-shell-initialize))
 
-;;;
-;;; terminal の PATH を引き継ぐ。
-;;;   参考: http://qiita.com/catatsuy/items/3dda714f4c60c435bb25
-;;;
-(exec-path-from-shell-initialize)
-
-(when (eq window-system 'ns) ;; Mac のみ。
+;; Mac のみ。
+(when (eq window-system 'ns)
   (setenv "PS1" "\\w$ ")
   (setenv "LANG" "ja_JP.UTF-8"))
