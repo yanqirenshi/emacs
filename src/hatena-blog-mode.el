7db96e04c1b445dfc418fdccbc5df2cce754906d
yanqirenshi <yanqirenshi@gmail.com>
2016-10-30 21:25:39 +0900

    modify to comment

--- src/hatena-blog-mode.el
+++ src/hatena-blog-mode.el
@@ -1,7 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d/dist/hatena-blog-mode/")
 (require 'hatena-blog-mode)
 
-(hatena-blog-add-blog "" ;blog-id
-                      "" ;name
-                      "" ;hatena-id
-                      "") ;api-key
+;; add init.el
+;; (hatena-blog-add-blog "" ;blog-id
+;;                       "" ;name
+;;                       "" ;hatena-id
+;;                       "") ;api-key
