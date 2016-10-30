f22fd1f889e8c57848ce83d080874ccc08e171e9
yanqirenshi <yanqirenshi@gmail.com>
2016-10-30 21:26:14 +0900

    use mode for *.html and *.tag

--- src/web-mode.el
+++ src/web-mode.el
@@ -1,4 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tag$" . web-mode))
 
 (defadvice web-mode-highlight-part (around tweak-jsx activate)
   (if (equal web-mode-content-type "jsx")
