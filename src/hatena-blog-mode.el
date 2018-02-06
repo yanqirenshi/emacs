4c117ba3b350f7da6328e4ff78a4a825f0bdfab8
yanqirenshi <yanqirenshi@gmail.com>
2018-02-06 11:03:24 +0900

    add hatena-blog-writer

--- src/hatena-blog-mode.el
+++
@@ -1,8 +0,0 @@
-(add-to-list 'load-path "~/.emacs.d/dist/hatena-blog-mode/")
-(require 'hatena-blog-mode)
-
-;; add init.el
-;; (hatena-blog-add-blog "" ;blog-id
-;;                       "" ;name
-;;                       "" ;hatena-id
-;;                       "") ;api-key
