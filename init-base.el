e2572b63248e16ea8a49ef2691cca35a14d6774c
yanqirenshi <yanqirenshi@gmail.com>
2018-03-02 01:53:34 +0000

    split init.el

---
+++ init-base.el
@@ -0,0 +1,41 @@
+;;;;; Contents
+;;;;; --------
+;;;;;  1. Mya Lisp library
+;;;;;  2. Setting Mode
+;;;;;
+
+;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;
+;;;;; 1. Mya Lisp library
+;;;;;
+;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+(load "~/.emacs.d/dist/emacs/lisp/my.el")
+(load "~/.emacs.d/dist/emacs/lisp/date.el")
+(load "~/.emacs.d/dist/emacs/lisp/dir-and-file.el")
+
+;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;
+;;;;; 2. Setting Mode
+;;;;;
+;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+(load "~/.emacs.d/dist/emacs/src/language.el")           ;; 言語/文字コード
+(load "~/.emacs.d/dist/emacs/src/environment.el")        ;; emacs の設定あれこれ
+(load "~/.emacs.d/dist/emacs/src/cursor.el")             ;; cursor の設定あれこれ
+(load "~/.emacs.d/dist/emacs/src/mode-line.el")          ;; mode-line の設定
+(load "~/.emacs.d/dist/emacs/src/picture-mode.el")       ;; picture-mode
+(load "~/.emacs.d/dist/emacs/src/org-mode.el")           ;; org-mode
+(load "~/.emacs.d/dist/emacs/src/theme.el")              ;; 色設定
+(load "~/.emacs.d/dist/emacs/src/buffer.el")             ;; buffer 設定
+(load "~/.emacs.d/dist/emacs/src/whitespace.el")         ;; whitespace 設定
+(load "~/.emacs.d/dist/emacs/src/package.el")            ;; package maneger
+(load "~/.emacs.d/dist/emacs/src/package-install.el")    ;; install packages
+(load "~/.emacs.d/dist/emacs/src/path.el")               ;; 環境変数の設定(OSの環境変数の引き継ぎ)
+(load "~/.emacs.d/dist/emacs/src/markdown-mode.el")      ;; markdown-mode
+(load "~/.emacs.d/dist/emacs/src/open-junk-file.el")     ;; open junk file
+(load "~/.emacs.d/dist/emacs/src/ddskk.el")              ;; ddskk
+(load "~/.emacs.d/dist/emacs/src/eww.el")                ;; eww
+(load "~/.emacs.d/dist/emacs/src/js2-mode.el")           ;; js2-mode
+(load "~/.emacs.d/dist/emacs/src/web-mode.el")           ;; web-mode
+(load "~/.emacs.d/dist/emacs/src/twittering-mode.el")    ;; twitter
+(load "~/.emacs.d/dist/emacs/src/ter-mode.el")           ;; T-ER Mode
+(load "~/.emacs.d/dist/emacs/src/git-gutter.el")         ;; git-gutter
---
+++ init-ruby.el
@@ -0,0 +1,3 @@
+(load "~/.emacs.d/dist/emacs/src/auto-complete.el")      ;; auto-complete
+(load "~/.emacs.d/dist/emacs/src/ruby-package-install.el")
+(load "~/.emacs.d/dist/emacs/src/ruby.el")
