7d327251393b0be8aafa21c88849dbdc12db03d3
yanqirenshi <yanqirenshi@gmail.com>
2018-05-18 16:11:13 +0900

    update init-ruby.el

--- init-ruby.el
+++ init-ruby.el
@@ -1,3 +1,6 @@
+;; https://qiita.com/tetsuo_jp/items/c05095931ae080f89d21
+(setq ruby-insert-encoding-magic-comment nil)
+
 (load "~/.emacs.d/dist/emacs/src/auto-complete.el")      ;; auto-complete
 (load "~/.emacs.d/dist/emacs/src/ruby-package-install.el")
 (load "~/.emacs.d/dist/emacs/src/ruby.el")
