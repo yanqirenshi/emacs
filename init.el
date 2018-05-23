3e6f67c9fedd5cdaded6a695cce220808ec5aa6a
yanqirenshi <yanqirenshi@gmail.com>
2018-05-23 11:00:12 +0900

    add init.template.el

--- init.el
+++
@@ -1,4 +0,0 @@
-(load "~/.emacs.d/dist/emacs/init-base.el")
-;; (load "~/.emacs.d/dist/emacs/init-blog.el")
-;; (load "~/.emacs.d/dist/emacs/init-lisp.el")
-;; (load "~/.emacs.d/dist/emacs/init-ruby.el")
---
+++ init.template.el
@@ -0,0 +1,5 @@
+(load "~/.emacs.d/dist/emacs/init-base.el")
+;;(load "~/.emacs.d/dist/emacs/init-blog.el")
+;;(load "~/.emacs.d/dist/emacs/init-lisp.el")
+;;(load "~/.emacs.d/dist/emacs/init-ruby.el")
+
