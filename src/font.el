43721414f4b837fbf19c17783f258c0731bb89c3
yanqirenshi <yanqirenshi@gmail.com>
2026-01-18 09:56:18 +0900

    modify font

--- src/font.el
+++ src/font.el
@@ -1,13 +1,20 @@
 ;;; init-environment.el --- Environment settings -*- lexical-binding: t; -*-
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family #("ＭＳ ゴシック" 0 7 (charset cp932-2-byte))
-                :foundry "outline"
-                :slant normal
-                :weight regular
-                :height 163
-                :width normal)))))
+(cond
+ ((member "みかちゃん" (font-family-list))
+  (custom-set-faces
+   '(default ((t (:family #("みかちゃん" 0 5 (charset cp932-2-byte))
+                          :foundry "outline"
+                          :slant normal
+                          :weight regular
+                          :height 163
+                          :width normal))))))
+ ((member "ＭＳ ゴシック" (font-family-list))
+  (custom-set-faces
+   '(default ((t (:family #("ＭＳ ゴシック" 0 7 (charset cp932-2-byte))
+                          :foundry "outline"
+                          :slant normal
+                          :weight regular
+                          :height 163
+                          :width normal))))))
+ (t (print "好みのフォントは存在しませんでした。")))
