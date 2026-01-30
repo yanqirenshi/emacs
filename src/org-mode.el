90ffab4f2e0f32e4c0d25e534cdb17409f11e735
yanqirenshi <yanqirenshi@gmail.com>
2026-01-30 21:54:14 +0900

    add git-auto-commit-mode

--- src/org-mode.el
+++ src/org-mode.el
@@ -14,3 +14,20 @@
           (lambda () (auto-fill-mode -1)))
 
 
+;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;
+;;;;; git-auto-commit-mode
+;;;;;
+;;;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package git-auto-commit-mode
+  :ensure t)
+
+;; 自動push
+(setq-default gac-automatically-push-p t)
+
+;; orgファイルで自動有効化
+(add-hook 'org-mode-hook
+          (lambda ()
+            (when (string-prefix-p (expand-file-name "~/org.notes/")
+                                   buffer-file-name)
+              (git-auto-commit-mode 1))))
