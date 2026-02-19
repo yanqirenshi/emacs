e2892e9c92d98d3cc488eec3c1c3ac00873b8d46
yanqirenshi <yanqirenshi@gmail.com>
2026-02-19 21:40:19 +0900

    Adds SLY configuration for Spinor Lisp
    Configures SLY to use the Spinor Lisp implementation, specifying
    the executable name, server name, and coding system. Sets Spinor
    Lisp as the default Lisp implementation for SLY.

---
+++ src/sly.el
@@ -0,0 +1,11 @@
+;;; init-environment.el --- Environment settings -*- lexical-binding: t; -*-
+
+(use-package sly
+  :ensure t
+  :defer t
+  :config
+  (add-to-list 'sly-lisp-implementations
+               '(spinor ("spinor" "server")
+                        :coding-system
+                        utf-8-unix))
+  (setq sly-default-lisp 'spinor))
